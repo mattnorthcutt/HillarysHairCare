3c6006fc7094f0c661a570fd428c0910bd1f5150
mattnorthcutt <matt_northcutt@icloud.com>
2025-10-13 19:48:30 -0500

    got the services to show up and got the vite proxy port to work it was a pain.

---
+++ client/src/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ServiceList from "./components/services/ServiceList";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="services" element={<ServiceList />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+);
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import ServiceList from "./components/services/ServiceList.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="services" element={<ServiceList />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
